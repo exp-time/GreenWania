8f9be76b024d576aa003b49adb41fa93a3eb2156
exp-time <166214763+exp-time@users.noreply.github.com>
2024-06-06 10:43:32 +0300

    test

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ function createLangButtons() {
   ];
   images.forEach((imgSrc, index) => {
     let button = new Elem({tag:'button',attrs:{className:`lang-btns`},children:[
-      {tag:'img',attrs:{className:'button display-topright'}, src: imgSrc}], parent: buttonContainer}).elem;
+      {tag:'img',attrs:{className:'cButton display-topright'}, src: imgSrc}], parent: buttonContainer}).elem;
     //button.style.left = `${index * -50}px`; // Adjust the position of each button
   });
 }
