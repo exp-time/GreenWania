dee9a7a0e4d63dded52f34979a49e9ea460fb5e3
exp-time <166214763+exp-time@users.noreply.github.com>
2024-06-06 09:34:13 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 function info_open(id) {document.getElementById(id).style.display = "block"}
 function info_close(id) {document.getElementById(id).style.display = "none"}
+
+document.addEventListener('DOMContentLoaded', function() { 
+   /* REMOVE */
+   document.getElementById('unfinished_modal').style.display='block'
+   /* REMOVE */
+ });
