c2bbcaaf043993accc3c3d5e0d91db08d6b39d4c
exp-time <166214763+exp-time@users.noreply.github.com>
2024-06-06 10:13:16 +0300

    test

---
+++ src/content/dynContent_fi.js
@@ -0,0 +1,20 @@
+const modalData = [
+  {
+    id: "contact_modal",
+    title: "Contact Information",
+    content: "",
+    footerContent: "",
+  },
+  {
+    id: "about_modal",
+    title: "About section",
+    content: "",
+    footerContent: ""
+  },
+  {
+    id: "unfinished_modal",
+    title: "Site Under Construction",
+    content: "The site is under construction. Contents subject to major change.",
+    footerContent: "",  
+  },
+];
