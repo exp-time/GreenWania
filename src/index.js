5e7ebcea21ed5e45a40ee0061d43f5014a38280a
exp-time <166214763+exp-time@users.noreply.github.com>
2024-06-16 11:46:07 +0300

    test

--- src/index.js
+++ src/index.js
@@ -103,15 +103,15 @@ function changeLang(language) {
 }
 
 function footerContents(lang) {
-  new Elem({ tag: 'div', attrs: { className: 'footer-container user-select-none' }, children: [
+  new Elem({ tag: 'div', attrs: { className: 'footer-container user-select-none padding-3' }, children: [
     new Elem({ tag: 'div', attrs: { className: 'footer-left' }, children: [
       { tag: 'p', attrs: { className: 'font-large', textContent: getText('#info1', lang) } },
       { tag: 'p', attrs: { className: 'font-large', textContent: getText('#info2', lang) } },
       { tag: 'p', attrs: { className: 'font-large', textContent: getText('#info3', lang) } }
     ]}),
     new Elem({tag: 'div', attrs: { className: 'footer-section' }, children: [
-      { tag: 'div', attrs: { className: 'button font-xlarge dark-green button-margin padding-3', textContent: getText('#contactBTN', lang), onclick: () => info_open('contact_modal') } },
-      { tag: 'div', attrs: { className: 'button font-xlarge dark-green button-margin padding-3', textContent: getText('#aboutBTN', lang), onclick: () => info_open('about_modal') } }
+      { tag: 'div', attrs: { className: 'button font-xlarge dark-green button-margin', textContent: getText('#contactBTN', lang), onclick: () => info_open('contact_modal') } },
+      { tag: 'div', attrs: { className: 'button font-xlarge dark-green button-margin', textContent: getText('#aboutBTN', lang), onclick: () => info_open('about_modal') } }
     ]}),
     new Elem({tag: 'div', attrs: { className: 'footer-right' }, children: [
       { tag: 'p', attrs: { className: 'font-large', innerHTML: '&copy; 2024 Paceville Oy. All rights reserved.' } }
