ff3f9da72db9694814c36f556c543d474960ff64
exp-time <166214763+exp-time@users.noreply.github.com>
2024-06-17 07:20:21 +0300

    test

--- src/index.js
+++ src/index.js
@@ -124,7 +124,7 @@ function footerContents(lang) {
 
 // Function to create the "back to top" button
 function createBackToTopButton() {
-  if (!document.querySelector('.btn-container2')) {
+  if (!document.querySelector('.topBtn')) {
     new Elem({tag: 'div', attrs: {className: 'button font-xxlarge dark-green corner-all btn-container2 topBtn',
     onclick: () => window.scrollTo({top: 0, behavior: 'smooth'})}, children:[
       {tag: 'div', attrs: {innerHTML: '&mapstoup;'}}], parent:document.body});
