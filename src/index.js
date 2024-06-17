828e30cb3f129cfb6aabea5e239ca0d0a43ce18d
exp-time <166214763+exp-time@users.noreply.github.com>
2024-06-17 08:01:55 +0300

    test

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ function createModal(id, title, content, footerContent) {
   } else {
     modalContent = new Elem({tag:'div',attrs:{className: 'modal-content card-4 modal-animate-top'},
       children: [headerWithClose(id, title, "modal-header font-xlarge"),
-        {tag: 'div', attrs: {className: 'padding-3'}, children:[
+        {tag: 'div', attrs: {className: 'padding'}, children:[
           {tag: 'p', attrs: {className:'font-large',textContent:content}}]}]}).elem;
     if (footerContent && footerContent != ""){ modalContent.appendChild(createFooter(footerContent)) }
   }
