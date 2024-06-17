15a267b1fbe0e0aa558e39e7d1b80cd2795de803
exp-time <166214763+exp-time@users.noreply.github.com>
2024-06-17 08:34:23 +0300

    test

--- src/index.js
+++ src/index.js
@@ -153,21 +153,6 @@ function createLangButtons() {
   });
 }
 
-function genContent(parentELement, content, tag, classList) {
-  if (typeof content === 'string') {
-    new Elem({tag: tag, attrs: {className: classList,textContent: content}, parent: parentELement});
-  } else if (typeof content === 'object') { 
-    for (const [key, value] of Object.entries(content)) { 
-      if (tag === "button") {
-        
-      }
-      else {
-        new Elem({tag: tag, attrs: {id: key, className: classList,textContent: value}, parent: parentELement});
-      }
-    } 
-  } else {console.error(`Content generation error to ${parentELement}`);}
-}
-
 function createNav(lang) {
   let navElem = document.getElementById('navbar')
   navElem.innerHTML = ''
